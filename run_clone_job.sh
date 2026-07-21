a454a3df0bea8a3c564bd8e5f1f2cab83c71432d
Ricardo Branco <rbranco@suse.de>
2026-07-21 16:17:56 +0200

    Use podman as root to drop rootless usage

--- run_clone_job.sh
+++ run_clone_job.sh
@@ -15,7 +15,7 @@ fi
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-compose_command=(podman run --rm -it -v $HOME/client.conf:/etc/openqa/client.conf:ro,z ghcr.io/ricardobranco777/openqa-cli /usr/share/openqa/script/clone_job.pl)
+compose_command=(sudo podman run --rm --user "$(id -u):$(id -g)" -it -v $HOME/client.conf:/etc/openqa/client.conf:ro,z ghcr.io/ricardobranco777/openqa-cli /usr/share/openqa/script/clone_job.pl)
 
 if [[ $1 =~ ^- ]] ; then
 	exec "${compose_command[@]}" "$@"
